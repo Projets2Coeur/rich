1c19bb495ef56d1ea0b9189c3471e3f7f3f7f962
Jérémy FRERE <frere.jeremy@gmail.com>
2015-02-19 17:44:28 +0100

    Fixes a bug with rich_input when calling simple_form without an object

--- app/inputs/rich_input.rb
+++ app/inputs/rich_input.rb
@@ -1,16 +1,18 @@
 if (Object.const_defined?("Formtastic") && Gem.loaded_specs["formtastic"].version.version[0,1] == "2")
 
-  class RichInput < ::Formtastic::Inputs::TextInput  
+  class RichInput < ::Formtastic::Inputs::TextInput
     def to_html
 
-      scope_type = object_name
-      scope_id = object.id
-      editor_options = Rich.options(options[:config], scope_type, scope_id)
+      if object
+        scope_type = object_name
+        scope_id = object.id
+        editor_options = Rich.options(options[:config], scope_type, scope_id)
+      end
 
       input_wrapping do
         label_html <<
         builder.text_area(method, input_html_options) <<
-        "<script>CKEDITOR.replace('#{dom_id}', #{editor_options.to_json.html_safe});</script>".html_safe  
+        "<script>CKEDITOR.replace('#{dom_id}', #{editor_options.to_json.html_safe});</script>".html_safe
       end
     end
   end
@@ -20,12 +22,14 @@ end
 if Object.const_defined?("SimpleForm")
   class RichInput < SimpleForm::Inputs::TextInput
     def input
-      scope_type = object_name
-      scope_id = object.id
-      editor_options = Rich.options(options[:config], scope_type, scope_id)
+      if object
+        scope_type = object_name
+        scope_id = object.id
+        editor_options = Rich.options(options[:config], scope_type, scope_id)
+      end
 
       super <<
-      "<script>CKEDITOR.replace('#{object_name}_#{attribute_name}', #{editor_options.to_json.html_safe});</script>".html_safe 
+      "<script>CKEDITOR.replace('#{object_name}_#{attribute_name}', #{editor_options.to_json.html_safe});</script>".html_safe
     end
   end
 end
